987eee143308227fdca4803f6d398e317d83be9e
IvanKoshel <educationZak@gmail.com>
2023-03-22 18:07:58 +0200

    Create user schema

--- service/schemas/contact.js
+++ service/schemas/contact.js
@@ -1,37 +1,42 @@
+const { ObjectId } = require("bson");
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const contact = new Schema(
+const contactSchema = new Schema(
   {
     name: {
       type: String,
       trim: true,
       minLength: 3,
       maxLength: 30,
-      require: true,
+      require: [true, "Name is required"],
     },
     email: {
       type: String,
       trim: true,
       lowercase: true,
-      unique: [true,'Duplicate email'],
-      require: true,
+      unique: [true, "Duplicate email"],
+      require: [true, "Email is required"],
     },
     phone: {
       type: String,
       trim: true,
-      require: true,
+      require: [true, "Phone is required"],
     },
     favorite: {
       type: Boolean,
-      select:true,
+      select: true,
       default: false,
       require: true,
+    },
+    owner: {
+      type: ObjectId,
+      ref: "user",
     },
   },
   { versionKey: false, timestamps: false }
 );
 
-const Contact = mongoose.model("contact", contact);
+const Contact = mongoose.model("contact", contactSchema);
 
 module.exports = Contact;
